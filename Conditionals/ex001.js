1f8bfd7b381093133aa61639208424136d2cedec
Yuran Porto <yuranporto@gmail.com>
2022-05-26 22:28:24 -0300

    Exercices, conditionals and Introduction in DOM

---
+++ Conditionals/ex001.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log('Drive safe aways')
+if(vel > 60) //simple conditional
+{
+    console.log('Mucth speed, you got a traffic ticket!')
+}
---
+++ Conditionals/ex002.js
@@ -0,0 +1,9 @@
+var country = 'Brazil'
+if(country == "Brazil")
+{
+    console.log('You are Brazilian')
+}
+else
+{
+    console.log('You are Foreing')
+}
